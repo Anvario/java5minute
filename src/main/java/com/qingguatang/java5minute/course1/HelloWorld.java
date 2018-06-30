f013569ece56853feafd9c879a82f1dab6bee4c2
Anvario <1441070789@qq.com>
2018-06-30 16:39:47 +0800

    test

--- src/main/java/com/qingguatang/java5minute/course1/HelloWorld.java
+++ src/main/java/com/qingguatang/java5minute/course1/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.qingguatang.java5minute.course1;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author 张宇
  */
 
-@RestController
+@Controller
 public class HelloWorld {
 
     @RequestMapping("/")
